c74ad0a7f3b904630ac20b75f4a9385962f6e93f
jordanferreirasaran <jfsaran@gmail.com>
2023-10-21 06:56:04 -0300

    Created strucuture to admin, serializers, controllers, data, models, migrations and tests.

--- products/models.py
+++ products/models.py
@@ -1,3 +1,39 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from vendor.mixins.model import ModelWithoutIdMixin
 
-# Create your models here.
+
+class Product(ModelWithoutIdMixin):
+
+    id = models.BigAutoField(primary_key=True,
+                             null=False,
+                             blank=False,
+                             editable=False,
+                             verbose_name="Código de barra")
+    product = models.CharField(max_length=255,
+                               blank=False,
+                               null=False,
+                               unique=True,
+                               verbose_name="Produto/Serviço")
+    unitary_price = models.DecimalField(blank=False,
+                                        null=False,
+                                        default=0.00,
+                                        verbose_name="Preço unitário",
+                                        max_digits=10,
+                                        decimal_places=2,
+                                        validators=[
+                                           MinValueValidator(0.00)
+                                       ])
+    commission = models.SmallIntegerField(blank=False,
+                                          null=False,
+                                          default=0,
+                                          verbose_name="Comissão",
+                                          validators=[
+                                              MinValueValidator(0),
+                                              MaxValueValidator(10)
+                                          ])
+
+    class Meta:
+        ordering = ['product', 'unitary_price', 'commission']
+        verbose_name = "product"
+        verbose_name_plural = "products"
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from typing import Any, Dict
 
-# Create your tests here.
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from products.models import Product
+
+
+class ProductTest(APITestCase):
+    basename: str = 'product'
+    fixtures = ["products.json"]
+    payload: Dict[str, Any] = {
+        "product": "Caderno de 10 materias",
+        "unitary_price": "19.90",
+        "commission": 10
+    }
+
+    def test_get_product_by_pk(self):
+        product: Product = Product.objects.get(pk=1)
+        self.assertEqual(product.product, "Caderno de Brochura")
+        self.assertEqual(product.unitary_price, "8.20")
+        self.assertEqual(product.commission, 5)
+
+    def test_list_products(self):
+        url = reverse(f'{self.basename}-list')
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_retrieve_product(self):
+        url = reverse(f'{self.basename}-detail', args=[1])
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_delete_product(self):
+        url = reverse(f'{self.basename}-detail', args=[1])
+        response = self.client.delete(url, format='json')
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_create_product(self):
+        url = reverse(f'{self.basename}-list')
+        response = self.client.post(url, self.payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        product: Product = Product.objects.filter(**self.payload).first()
+        if product:
+            self.assertEqual(product.product, "Caderno de 10 materias")
+            self.assertEqual(product.unitary_price, "19.90")
+            self.assertEqual(product.commission, 10)
+
+    def test_unique_product(self):
+        url = reverse(f'{self.basename}-list')
+        response = self.client.post(url, self.payload, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
