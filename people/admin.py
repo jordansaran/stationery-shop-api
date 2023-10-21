4a1e2fd555f4d411794047c5cbff6e23e91fcf6d
jordanferreirasaran <jfsaran@gmail.com>
2023-10-21 06:56:45 -0300

    Created strucuture to admin, serializers, controllers, data, models, migrations and tests to People.

--- people/admin.py
+++ people/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from people.models import Seller, Client
+
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+    ordering = ['name', 'email', 'phone']
+    search_fields = ['name', 'email', 'phone']
+    list_filter = ['email']
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone']
+    ordering = ['name', 'email', 'phone']
+    search_fields = ['name', 'email', 'phone']
+    list_filter = ['email']
+
+
+admin.site.register(Seller, SellerAdmin)
+admin.site.register(Client, ClientAdmin)
--- people/models.py
+++ people/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from vendor.mixins.model import ModelWithoutIdMixin
+
+
+class People(ModelWithoutIdMixin):
+    id = models.BigAutoField(primary_key=True,
+                             blank=False,
+                             null=False,
+                             editable=False,
+                             verbose_name="Id")
+    name = models.CharField(max_length=50,
+                            blank=False,
+                            null=False,
+                            verbose_name="Nome")
+    email = models.EmailField(blank=False,
+                              null=False,
+                              unique=True,
+                              verbose_name="Email")
+    phone = models.BigIntegerField(max_length=11,
+                                   null=False,
+                                   blank=False,
+                                   unique=True,
+                                   verbose_name="Telefone")
+
+    def __str__(self):
+        return f"{str(self.id).zfill(3)} - {self.name}"
+
+    class Meta:
+        ordering = ['name', 'email', 'phone']
+        verbose_name = 'person'
+        verbose_name_plural = 'people'
+
+
+class Seller(People):
+
+    class Meta:
+        verbose_name = 'seller'
+        verbose_name_plural = 'sellers'
+
+
+class Client(People):
+
+    class Meta:
+        verbose_name = 'client'
+        verbose_name_plural = 'customers'
